d04c43957421387f0e266321a9bacdd22ba5448c
Céline Foucart <celinefoucart@yahoo.fr>
2022-03-07 16:40:50 +0100

    Mise en place du profil

--- src/Controller/User/ProfileController.php
+++ src/Controller/User/ProfileController.php
@@ -2,17 +2,53 @@
 
 namespace App\Controller\User;
 
+use App\Entity\User;
+use App\Form\AccountType;
+use App\Form\ChangePasswordType;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Routing\Annotation\Route;
 
 class ProfileController extends AbstractController
 {
     #[Route('/profile', name: 'app_profile')]
-    public function index(): Response
-    {
+    public function index(
+        EntityManagerInterface $entityManager,
+        Request $request,
+        UserPasswordHasherInterface $userPasswordHasher
+    ): Response {
+        $user = $this->getUser();
+        assert($user instanceof User);
+        $accountForm = $this->createForm(AccountType::class, $user);
+        $accountForm->handleRequest($request);
+        
+        if ($accountForm->isSubmitted() && $accountForm->isValid()) { 
+            $entityManager->flush();
+            $this->addFlash('success', 'Vos informations ont été mises à jour.');
+            return $this->redirectToRoute('app_profile');
+        }
+
+        $passwordForm = $this->createForm(ChangePasswordType::class, $user);
+        $passwordForm->handleRequest($request);
+        
+        if ($passwordForm->isSubmitted() && $passwordForm->isValid()) { 
+            $user->setPassword(
+            $userPasswordHasher->hashPassword(
+                    $user,
+                    $passwordForm->get('plainPassword')->getData()
+                )
+            );
+            $entityManager->flush();
+            $this->addFlash('success', 'Votre mot de passe a été mis à jour.');
+            return $this->redirectToRoute('app_profile');
+        }
+
         return $this->render('user/profile.html.twig', [
-            'controller_name' => 'ProfileController',
+            'accountForm' => $accountForm->createView(),
+            'passwordForm' => $passwordForm->createView(),
         ]);
     }
 }
